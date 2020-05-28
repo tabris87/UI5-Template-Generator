d84d44cce7948113e1f679cf6134637feceebad3
Adrian Marten <Adrian.Marten@msg.group>
2020-05-28 15:45:13 +0200

    Finished first tree visualisation

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const colors = require('colors');
 
 const reader = xmlReader.create();
 
-const model = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
+const model =
+    '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
     '<featureModel>' +
     '    <properties/>' +
     '    <struct>' +
@@ -11,26 +12,40 @@ const model = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
     '            <feature mandatory="true" name="Base"/>' +
     '            <alt abstract="true" mandatory="true" name="CodeBase">' +
     '                <and name="Application">' +
-    '                    <alt abstract="true" mandatory="true" name="Layout">' +
-    '                        <feature name="Single"/>' +
-    '                        <feature name="MasterDetail"/>' +
-    '                        <feature name="FlexibleColumnLayout"/>' +
+    '                    <alt abstract="true" mandatory="true" name="Navigation">' +
+    '                        <alt name="Routing">' +
+    '                            <and abstract="true" name="Simple">' +
+    '                                <and mandatory="true" name="OnePage">' +
+    '                                    <and name="TwoPage">' +
+    '                                        <feature name="ThreePage"/>' +
+    '                                    </and>' +
+    '                                </and>' +
+    '                            </and>' +
+    '                            <alt name="MasterDetail">' +
+    '                                <feature name="WithDummyDetail"/>' +
+    '                                <feature name="WithEmptyDetail"/>' +
+    '                            </alt>' +
+    '                            <alt name="FlexibleColumnLayout">' +
+    '                                <feature name="TwoPageStartOne"/>' +
+    '                                <feature name="MasterDetailType"/>' +
+    '                            </alt>' +
+    '                        </alt>' +
+    '                        <feature name="NoRouting"/>' +
     '                    </alt>' +
-    '                    <alt abstract="true" mandatory="true" name="FirstPage">' +
-    '                        <feature name="FreeStyleFirst"/>' +
-    '                        <feature name="List"/>' +
-    '                        <feature name="Table"/>' +
-    '                        <feature name="WorkList"/>' +
-    '                        <feature name="Toolpage"/>' +
-    '                    </alt>' +
-    '                    <alt abstract="true" name="SecondPage">' +
-    '                        <feature name="FreeStyleSecond"/>' +
-    '                        <feature name="ObjectpageSecond"/>' +
-    '                    </alt>' +
-    '                    <alt abstract="true" name="ThirdPage">' +
-    '                        <feature name="FreeStyleThird"/>' +
-    '                        <feature name="ObjectpageThird"/>' +
-    '                    </alt>' +
+    '                    <and mandatory="true" name="FirstPage">' +
+    '                        <alt abstract="true" name="Pattern">' +
+    '                            <feature name="Table"/>' +
+    '                            <feature name="List"/>' +
+    '                            <feature name="Worklist"/>' +
+    '                            <feature name="Toolpage"/>' +
+    '                        </alt>' +
+    '                    </and>' +
+    '                    <and name="SecondPage">' +
+    '                        <feature name="ObjectPageSecond"/>' +
+    '                    </and>' +
+    '                    <and name="ThirdPage">' +
+    '                        <feature name="ObjectPageThird"/>' +
+    '                    </and>' +
     '                </and>' +
     '                <feature name="Library"/>' +
     '            </alt>' +
@@ -38,9 +53,36 @@ const model = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
     '    </struct>' +
     '    <constraints>' +
     '        <rule>' +
+    '            <description>Without routing we can not have a third or even a second page.</description>' +
     '            <imp>' +
+    '                <var>NoRouting</var>' +
+    '                <not>' +
+    '                    <disj>' +
+    '                        <var>SecondPage</var>' +
+    '                        <var>ThirdPage</var>' +
+    '                    </disj>' +
+    '                </not>' +
+    '            </imp>' +
+    '        </rule>' +
+    '        <rule>' +
+    '            <imp>' +
+    '                <var>TwoPage</var>' +
+    '                <var>SecondPage</var>' +
+    '            </imp>' +
+    '        </rule>' +
+    '        <rule>' +
+    '            <imp>' +
+    '                <var>ThreePage</var>' +
     '                <var>ThirdPage</var>' +
-    '                <var>FlexibleColumnLayout</var>' +
+    '            </imp>' +
+    '        </rule>' +
+    '        <rule>' +
+    '            <imp>' +
+    '                <var>MasterDetail</var>' +
+    '                <conj>' +
+    '                    <var>FirstPage</var>' +
+    '                    <var>SecondPage</var>' +
+    '                </conj>' +
     '            </imp>' +
     '        </rule>' +
     '    </constraints>' +
@@ -51,20 +93,116 @@ const model = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
 
 const contains = (oObj, sAttribute, sDefault = "") => oObj[sAttribute] ? oObj[sAttribute] : sDefault;
 
-const traverse = (node, level, print) => {
+const firstPrefix = (bAlt, bOnly, bLastIndex) => {
+    if (bAlt) {
+        if (bOnly || bLastIndex) {
+            return String.fromCharCode(9562); //"╚"
+        } else {
+            return String.fromCharCode(9568); //"⊫"
+        }
+    } else {
+        if (bOnly || bLastIndex) {
+            return String.fromCharCode(9492) //"˪"
+        } else {
+            return String.fromCharCode(9500) //"⊢"
+        }
+    }
+}
+
+const selectionField = (bAlt, bMandatory) => {
+    if (bAlt) {
+        if (bMandatory) {
+            return colors.green('(0)');
+        } else {
+            return colors.red('( )');
+        }
+    } else {
+        if (bMandatory) {
+            return colors.green('|X|');
+        } else {
+            return colors.red('| |');
+        }
+    }
+}
+
+const createParentPrefix = (node) => {
+    if (node.parent) {
+        let sParentPrefix = createParentPrefix(node.parent);
+
+        if (contains(node.attributes, "name", "") === "Application") debugger;
+        let sCurPrefix = node.parent.prefix;
+
+        switch (sCurPrefix) {
+            case String.fromCharCode(9562):
+                debugger;
+                sCurPrefix = " ";
+                break;
+            case String.fromCharCode(9492):
+                debugger;
+                sCurPrefix = " ";
+                break;
+            case String.fromCharCode(9568):
+                debugger;
+                sCurPrefix = String.fromCharCode(9553);
+                break;
+            case String.fromCharCode(9500):
+                debugger;
+                sCurPrefix = "|";
+                break;
+            default:
+                debugger;
+                sCurPrefix = sCurPrefix;
+        }
+
+        /* sParentPrefix = sParentPrefix === String.fromCharCode(9562) ? " " : sParentPrefix;
+        sParentPrefix = sParentPrefix === String.fromCharCode(9492) ? " " : sParentPrefix;
+        sParentPrefix = sParentPrefix === String.fromCharCode(8875) ? String.fromCharCode(9553) : node.prefix; // "║"
+        sParentPrefix = sParentPrefix === String.fromCharCode(9500) ? "|" : sParentPrefix; */
+
+        return sParentPrefix + (sCurPrefix ? sCurPrefix : "");
+    } else {
+        return "";
+    }
+}
+
+const traverse = (node, level = -1, print = false, childIndex = -1) => {
     //startPrinting
     if (node.name === "struct") {
-        node.children.forEach((subNode) => traverse(subNode, ++level, true));
+        level++;
+        node.prefix = " ";
+        node.children.forEach((subNode, index) => traverse(subNode, level, true, index));
         return
     }
 
     if (print) {
-        process.stdout.write(`Level ${level}: ${node.name} -> ${contains(node.attributes, "name", "unnamed")}\n`);
-        debugger;
+        let parentPrefix = createParentPrefix(node);
+        let prefix = firstPrefix(node.parent.name === "alt", node.parent.children.length === 1, node.parent.children.length - 1 === childIndex);
+        let selectField = selectionField(node.parent.name === "alt", contains(node.attributes, "mandatory", false));
+        let featureName = contains(node.attributes, "name", "unnamed");
+        let output = contains(node.attributes, "mandatory", undefined) ? colors.underline.cyan(featureName) : featureName;
+        output = contains(node.attributes, "abstract", undefined) ? colors.gray(featureName) : output;
+
+
+        let outputString = `${parentPrefix}${level!==0 ? prefix : ""}${level!==0 ? selectField : ""}${output}`;
+
+        node.prefix = prefix;
+        console.log(outputString);
     }
 
-    node.children.forEach((subNode) => traverse(subNode, level === 0 ? 0 : level++, print));
+    if (level > -1) {
+        level++;
+    }
+    node.children.forEach((subNode, index) => traverse(subNode, level === 0 ? 0 : level, print, index));
 };
 
-reader.on("done", data => traverse(data, 0, false));
+reader.on("done", data => {
+    traverse(data)
+
+    console.log('\n');
+
+    console.log(`Mandatory:   ${colors.cyan.underline('<FeatureName>')}`);
+    console.log(`Abstract:    ${colors.gray('<FeatureName>')}`);
+    console.log(`Optional:    ${colors.red('| |')} <- unselected, ${colors.green('|X|')} <- selected`);
+    console.log(`Alternative: '${String.fromCharCode(9562)}','${String.fromCharCode(9568)}' - ${colors.red('( )')} <- unselected, ${colors.green('(0)')} <- selected`);
+});
 reader.parse(model);
