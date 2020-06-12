72dfdc90a56dcb247ccb41b276565751715dc848
Adrian Marten <Adrian.Marten@msg.group>
2020-06-12 09:03:48 +0200

    Added build scenario for Applications

--- features/Grunt_Build_Application/Gruntfile.js
+++ features/Grunt_Build_Application/Gruntfile.js
@@ -12,14 +12,77 @@ function setupConfig(grunt) {
                 },
                 components: 'fop.template.ui5'
             }
+        },
+        cssmin: {
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: 'webapp',
+                    src: 'css/*.css',
+                    dest: 'dist'
+                }]
+            }
+        },
+        copy: {
+            component: {
+                files: [{
+                        expand: true,
+                        cwd: 'webapp',
+                        src: ['*.js', '**/*.js', '!test/**'],
+                        dest: 'dist',
+                        rename: function (dest, src) {
+                            return dest + '/' + src.replace('.', "-dbg.");
+                        }
+                    },
+                    {
+                        expand: true,
+                        cwd: 'webapp',
+                        src: [
+                            '**/*.jpg',
+                            '**/*.jpeg',
+                            '**/*.svg',
+                            '**/*.png',
+                            '**/*.bmp',
+                            '**/*.gif',
+                            '**/*.css',
+                            '**/*.html',
+                            '**/*.json',
+                            '**/*.xml',
+                            '**/*.properties',
+                            'manifest.json',
+                            '!test/**',
+                            '!*_mock.html'
+                        ],
+                        dest: 'dist'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            component: {
+                files: [{
+                    expand: true,
+                    src: [
+                        '*.js',
+                        '**/*.js',
+                        '!test/**'
+                    ],
+                    dest: 'dist',
+                    cwd: 'webapp'
+                }]
+            }
         }
     });
 }
 
 function loadNPMTasks(grunt) {
     original(grunt);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 }
 
 function registerTasks(grunt) {
+    grunt.registerTask('build', ['cssmin:build', 'openui5_preload:component', 'uglify:component', 'copy:component']);
     original(grunt);
 }
